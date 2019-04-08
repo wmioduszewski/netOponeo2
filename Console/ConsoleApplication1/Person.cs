b3015edc7c7f851e5c4a0e9907d0c1a86a9a37ec
Wojciech Mioduszewski <wojciech.mioduszewski@sienn.pl>
2019-04-08 20:48:20 +0200

    web app,

--- Console/ConsoleApplication1/Person.cs
+++ Console/ConsoleApplication1/Person.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Person
+    class Person : ICloneable
     {
         public int Age { get; set; }
         public string Name { get; set; }
 
+        public List<string> Colors { get; set; }
+
         public override string ToString()
         {
             return Name + " is " + Age + " years old";
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Console/ConsoleApplication1/Program.cs
+++ Console/ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace ConsoleApplication1
 {
@@ -10,13 +11,49 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            //Metoda();
 
-            int a = Convert.ToInt32(Console.ReadLine());
-            int A = Convert.ToInt32(Console.ReadLine());
+            //var people = People();
 
-            Console.WriteLine(a/A);
+            //var result = people.GroupBy(x=>x.Name).Distinct();
+
+            //Person p1 = new Person(){Age = 5, Name = "Filip"};
+            //Person p2 = p1.;
+
+            int[] a1 = new[] {1, 2, 3};
+            int[] a2 = (int[])a1.Clone();
+            a2[0] = 5;
+
+            Console.WriteLine(string.Join(",", a1));
+            Console.WriteLine(string.Join(",", a2));
+
+            Console.WriteLine("Przed zmianą");
+            //Console.WriteLine(p1.Age);
+            //Console.WriteLine(p2.Age);
+
+            //p2.Age = 6;
+
+            Console.WriteLine("Po zmianie");
+            Console.ReadKey();
+            //Console.WriteLine(p1.Age);
+            //Console.WriteLine(p2.Age);
+
 
             Console.ReadKey();
+        }
+
+        private static int Metoda()
+        {
+            return Metoda();
+        }
+
+        private static List<Person> People()
+        {
+            return new List<Person>() {
+                new Person() { Colors = new List<string>(){"blue", "green"}, Age= 5, Name= "Jake" } ,
+                new Person() { Colors = new List<string>(){"orange"}, Age= 1, Name= "Ada" } ,
+                new Person() { Colors = new List<string>(){"white", "indigo", "orange"}, Age= 2, Name= "Ada" } ,
+                new Person() { Colors = new List<string>(){"black", "magenta", "blue"}, Age= 7, Name= "Mel" } };
         }
     }
 }
